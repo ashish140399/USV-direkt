02b2b2b017fe6c9357fe1f042049f28c8b5845f8
ashish140399 <ashish140399@gmail.com>
2019-09-15 15:32:58 +0530

    15first

--- wp-content/themes/shoppingcart/footer.php
+++ wp-content/themes/shoppingcart/footer.php
@@ -15,65 +15,59 @@ $shoppingcart_settings = shoppingcart_get_theme_options(); ?>
 
 $footer_column = $shoppingcart_settings['shoppingcart_footer_column_section'];
 	if( is_active_sidebar( 'shoppingcart_footer_1' ) || is_active_sidebar( 'shoppingcart_footer_2' ) || is_active_sidebar( 'shoppingcart_footer_3' ) || is_active_sidebar( 'shoppingcart_footer_4' )) { ?>
-	<div class="widget-wrap">
-		<div class="wrap">
-			<div class="widget-area">
-			<?php
-				if($footer_column == '1' || $footer_column == '2' ||  $footer_column == '3' || $footer_column == '4'){
-				echo '<div class="column-'.absint($footer_column).'">';
-					if ( is_active_sidebar( 'shoppingcart_footer_1' ) ) :
-						dynamic_sidebar( 'shoppingcart_footer_1' );
-					endif;
-				echo '</div><!-- end .column'.absint($footer_column). '  -->';
-				}
-				if($footer_column == '2' ||  $footer_column == '3' || $footer_column == '4'){
-				echo '<div class="column-'.absint($footer_column).'">';
-					if ( is_active_sidebar( 'shoppingcart_footer_2' ) ) :
-						dynamic_sidebar( 'shoppingcart_footer_2' );
-					endif;
-				echo '</div><!--end .column'.absint($footer_column).'  -->';
-				}
-				if($footer_column == '3' || $footer_column == '4'){
-				echo '<div class="column-'.absint($footer_column).'">';
-					if ( is_active_sidebar( 'shoppingcart_footer_3' ) ) :
-						dynamic_sidebar( 'shoppingcart_footer_3' );
-					endif;
-				echo '</div><!--end .column'.absint($footer_column).'  -->';
-				}
-				if($footer_column == '4'){
-				echo '<div class="column-'.absint($footer_column).'">';
-					if ( is_active_sidebar( 'shoppingcart_footer_4' ) ) :
-						dynamic_sidebar( 'shoppingcart_footer_4' );
-					endif;
-				echo '</div><!--end .column'.absint($footer_column).  '-->';
-				}
-				?>
-			</div> <!-- end .widget-area -->
-		</div><!-- end .wrap -->
+	<div class="widget-wrap" style="padding: 3% 5%">
+		<div class="row">
+			<div class="col-sm-5">
+				<img src="http://localhost/direkt/wp-content/uploads/2019/09/USV-Direkt-Logo-Small.png" style="width:40%;padding: 5%">
+			</div>
+			<div class="col-sm-7">
+				<div class="row">
+					<div class="col-sm-4">
+						<div style="font-size: 19px">KUNDEN SERVICE</div>
+						<div>
+							Hilfe <br>
+							Konaktiere uns <br>
+							Garantie <br>
+							Geschäftsbedingungen <br>
+							AGB
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div style="margin-top: 20px">
+							Datenschutz <br>
+							FAQ <br>
+							Impressum
+						</div>
+						
+					</div>
+					<div class="col-sm-4">
+						<div style="font-size: 19px">ÜBER UNSERE</div>
+						<div>
+							Partner <br>
+							Projkete <br>
+							Dienste <br>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div> <!-- end .widget-wrap -->
 	<?php } ?>
-	<div class="site-info">
-	<div class="wrap">
-	<?php do_action('shoppingcart_footer_menu');
-	if($shoppingcart_settings['shoppingcart_buttom_social_icons'] == 0):
-		do_action('shoppingcart_social_links');
-	endif;
-
-	if ( is_active_sidebar( 'shoppingcart_footer_options' ) ) :
-		dynamic_sidebar( 'shoppingcart_footer_options' );
-	else:
-		echo '<div class="copyright">'; ?>
-		<a title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" target="_blank" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo get_bloginfo( 'name', 'display' ); ?></a> | 
-						<?php esc_html_e('Designed by:','shoppingcart'); ?> <a title="<?php echo esc_attr__( 'Theme Freesia', 'shoppingcart' ); ?>" target="_blank" href="<?php echo esc_url( 'https://themefreesia.com' ); ?>"><?php esc_html_e('Theme Freesia','shoppingcart');?></a> |
-						<?php  echo '&copy; ' . date_i18n(__('Y','shoppingcart')) ; ?> <a title="<?php echo esc_attr__( 'WordPress', 'shoppingcart' );?>" target="_blank" href="<?php echo esc_url( 'https://wordpress.org' );?>"><?php esc_html_e('WordPress','shoppingcart'); ?></a>
-					</div>
-	<?php endif; ?>
-			<div style="clear:both;"></div>
+	<div class="site-info">	
+		<div class="wrap d-flex justify-content-between">
+			<div>Copyright &copy; 2018 <span style="color:#15A4FA">Patrick Butke & USV-Direkt.</span>All Rights Reserved.</div>
+			<div class="footer_social_icon">
+				<img src="http://localhost/direkt/wp-content/uploads/2019/09/Facebook-B-copy.png">
+				<img src="http://localhost/direkt/wp-content/uploads/2019/09/Twitter-B_w.png">
+				<img src="http://localhost/direkt/wp-content/uploads/2019/09/Instagram_w.png">
+				<img src="http://localhost/direkt/wp-content/uploads/2019/09/Linkedin-B_w.png">
+				<img src="http://localhost/direkt/wp-content/uploads/2019/09/Behance_w.png">
+			</div>
 		</div> <!-- end .wrap -->
 	</div> <!-- end .site-info -->
 	<?php
 		$disable_scroll = $shoppingcart_settings['shoppingcart_scroll'];
-		if($disable_scroll == 0):?>
+		if(1):?>
 			<button type="button" class="go-to-top" type="button">
 				<span class="screen-reader-text"><?php esc_html_e('Go to top','shoppingcart'); ?></span>
 				<span class="icon-bg"></span>
--- wp-content/themes/shoppingcart/header.php
+++ wp-content/themes/shoppingcart/header.php
@@ -9,6 +9,14 @@
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<!----ashish added--->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!----ashish ended--->
 <?php
 $shoppingcart_settings = shoppingcart_get_theme_options(); ?>
 <head>
